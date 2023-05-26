bb75defc3242c23715ba1607e50b7ce7b911123d
Ori Shefer <96699001+OriShefer@users.noreply.github.com>
2023-05-26 12:35:35 +0300

    change import chart.js

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -7,7 +7,7 @@ import {
   Title,
   Tooltip,
   Legend,
-} from "chart.js";
+} from "chart.js/auto";
 
 import { Bar } from "react-chartjs-2";
 import { useGlobalContext } from "../../Context/GlobalContext";
